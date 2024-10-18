38e5b324a4a2cf0954a6e57f803a2dce9d2f543a
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2024-10-17 21:25:31 -0400

    Create Constants.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,12 @@
+public final class Constants {
+  
+  public static class DriveConstants {
+  }
+  
+  public static class ArmConstants {
+  }
+
+  public static class AutonomousConstants {
+  }
+  
+}
