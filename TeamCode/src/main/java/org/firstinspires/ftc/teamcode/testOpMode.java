ea1cfb825abddef0a128ff0e48721f6e519cf1c0
Françoise Yen <francoiseyen@icloud.com>
2024-11-09 11:55:19 -0500

    Updated hardware initialization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOpMode.java
@@ -1,17 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.initialize.Hardware;
 
+@TeleOp
 public class testOpMode extends OpMode {
     @Override
     public void init() {
-        Hardware hardware = new Hardware(this);
         init();
     }
 
     public void loop() {
+        telemetry.addData("Status", "Running");
+        Hardware hardware = new Hardware(this);
+        double strafePwr;
+        double leftPwr = -gamepad1.left_stick_y;
+        double rightPwr = -gamepad1.right_stick_y;
 
+        if(gamepad1.dpad_left) {
+            strafePwr = 0.5;
+        }
+        else if(gamepad1.dpad_right) {
+            strafePwr = -0.5;
+        }
+        else {strafePwr = 0;}
+
+        hardware.strafe(strafePwr);
+        hardware.setDrivePower(leftPwr, rightPwr);
+        telemetry.addData("D-Pad left", gamepad1.dpad_left);
+        telemetry.addData("D-Pad right", gamepad1.dpad_right);
+        telemetry.addData("Left joystick y", gamepad1.left_stick_y);
+        telemetry.addData("Right joystick y", gamepad1.right_stick_y);
+        telemetry.addData("Strafe power", strafePwr);
+        telemetry.addData("Left power", leftPwr);
+        telemetry.addData("Right power", rightPwr);
+        telemetry.update();
     }
 }
