4986b76c9dcb142305023f8971a3573b3a40ddbd
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2024-11-09 13:51:19 -0500

    added commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOpMode.java
@@ -10,25 +10,28 @@ public class testOpMode extends OpMode {
 
     @Override
     public void init() {
-        init();
+        Hardware hardware = new Hardware(this);
+
+        double leftPwr,
+                rightPwr,
+                strafePwr;
     }
 
     public void loop() {
         telemetry.addData("Status", "Running");
 
-        Hardware hardware = new Hardware(this);
+        leftPwr = -gamepad1.left_stick_y;
+        rightPwr = -gamepad1.right_stick_y;
+        strafePwr;
 
-        double strafePwr;
-        double leftPwr = -gamepad1.left_stick_y;
-        double rightPwr = -gamepad1.right_stick_y;
-
-        if(gamepad1.dpad_left) {
+        if (gamepad1.dpad_left) {
             strafePwr = 0.5;
         }
-        else if(gamepad1.dpad_right) {
+        else if (gamepad1.dpad_right) {
             strafePwr = -0.5;
         }
-        else {strafePwr = 0;}
+        else {
+            strafePwr = 0;}
 
         hardware.strafe(strafePwr);
         hardware.setDrivePower(leftPwr, rightPwr);
