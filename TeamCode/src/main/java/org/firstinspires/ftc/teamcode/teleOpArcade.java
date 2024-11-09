43fd371e910584e77c186119867ceac9def8bcde
François Ian Yen <francoisianyen@icloud.com>
2024-11-09 14:22:45 -0500

    Created arcade drive mode, updated tank drive mode. Improved initialization.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpArcade.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpArcade.java
@@ -6,23 +6,22 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.initialize.Hardware;
 
 @TeleOp
-public class testOpMode extends OpMode {
+public class teleOpArcade extends OpMode {
+    Hardware hardware = new Hardware(this);
+
+    double drive, turn, strafePwr;
 
     @Override
     public void init() {
-        Hardware hardware = new Hardware(this);
-
-        double leftPwr,
-                rightPwr,
-                strafePwr;
+        hardware.init();
     }
 
+    @Override
     public void loop() {
         telemetry.addData("Status", "Running");
 
-        leftPwr = -gamepad1.left_stick_y;
-        rightPwr = -gamepad1.right_stick_y;
-        strafePwr;
+        drive = -gamepad1.left_stick_y;
+        turn = gamepad1.right_stick_x;
 
         if (gamepad1.dpad_left) {
             strafePwr = 0.5;
@@ -34,15 +33,15 @@ public class testOpMode extends OpMode {
             strafePwr = 0;}
 
         hardware.strafe(strafePwr);
-        hardware.setDrivePower(leftPwr, rightPwr);
+        hardware.driveArcade(drive, turn);
 
         telemetry.addData("D-Pad left", gamepad1.dpad_left);
         telemetry.addData("D-Pad right", gamepad1.dpad_right);
         telemetry.addData("Left joystick y", gamepad1.left_stick_y);
         telemetry.addData("Right joystick y", gamepad1.right_stick_y);
         telemetry.addData("Strafe power", strafePwr);
-        telemetry.addData("Left power", leftPwr);
-        telemetry.addData("Right power", rightPwr);
+        telemetry.addData("Drive", drive);
+        telemetry.addData("Turn", turn);
         telemetry.update();
     }
 }
