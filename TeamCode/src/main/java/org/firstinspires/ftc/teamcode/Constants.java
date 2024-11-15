13865e15cf304fc97219109826d58e7aa68da3ee
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2024-11-14 23:01:29 -0500

    organized functions into their respective classes, all of which are located under the commands/teleOpCommands/drive
    changed motor classes to DcMotorEx for extra functionality, such as velocity recording
    
    added motor braking function to all motors for when there is no power being sent to a specific motor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,6 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
 public final class Constants {
   
   public static class DriveConstants {
+    static final protected double strafeMultiplier = 0.5,
+                                  driveMultiplier = 0.7;
+
   }
   
   public static class ArmConstants {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initialize/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initialize/Hardware.java
@@ -2,13 +2,14 @@ package org.firstinspires.ftc.teamcode.initialize;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class Hardware {
 
     protected OpMode callingOpMode;
 
-    protected DcMotor leftDrive,
+    protected DcMotorEx leftDrive,
                         rightDrive,
                         transverseDrive;
 
@@ -28,13 +29,17 @@ public class Hardware {
         callingOpMode.telemetry.addData("Status", "Initialize");
         callingOpMode.telemetry.update();
 
-        leftDrive = callingOpMode.hardwareMap.get(DcMotor.class, "leftDrive");
-        rightDrive = callingOpMode.hardwareMap.get(DcMotor.class, "rightDrive");
-        transverseDrive = callingOpMode.hardwareMap.get(DcMotor.class, "transverseDrive");
+        leftDrive = callingOpMode.hardwareMap.get(DcMotorEx.class, "leftDrive");
+        rightDrive = callingOpMode.hardwareMap.get(DcMotorEx.class, "rightDrive");
+        transverseDrive = callingOpMode.hardwareMap.get(DcMotorEx.class, "transverseDrive");
 
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
         transverseDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        transverseDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
