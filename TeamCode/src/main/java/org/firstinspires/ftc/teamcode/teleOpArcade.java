54c3c65c01ba2789bc78f8d237b0a5b1109923d9
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2024-11-14 23:01:29 -0500

    organized functions into their respective classes, all of which are located under the commands/teleOpCommands/drive
    changed motor classes to DcMotorEx for extra functionality, such as velocity recording
    
    added motor braking function to all motors for when there is no power being sent to a specific motor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpArcade.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpArcade.java
@@ -14,10 +14,9 @@ public class teleOpArcade extends OpMode {
     private Strafe m_strafe = new Strafe();
 
     private DriveArcade m_driveArcade = new DriveArcade();
-    
-    private double drive,
-                    turn,
-                    strafePwr;
+    double drive,
+            turn,
+            strafePwr;
 
     @Override
     public void init() {
