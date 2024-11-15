ac9f43883c06b278c39d60ba35f6ab370d18433e
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2024-11-14 22:31:54 -0500

    organized functions into their respective classes, all of which are located under the commands/teleOpCommands/drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpArcade.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpArcade.java
@@ -3,13 +3,20 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.commands.teleopCommands.drive.DriveArcade;
+import org.firstinspires.ftc.teamcode.commands.teleopCommands.drive.Strafe;
 import org.firstinspires.ftc.teamcode.initialize.Hardware;
 
 @TeleOp
 public class teleOpArcade extends OpMode {
-    Hardware hardware = new Hardware(this);
+    private Hardware hardware = new Hardware(this);
 
-    double drive, turn, strafePwr;
+    private Strafe m_strafe = new Strafe();
+
+    private DriveArcade m_driveArcade = new DriveArcade();
+    double drive,
+            turn,
+            strafePwr;
 
     @Override
     public void init() {
@@ -23,17 +30,10 @@ public class teleOpArcade extends OpMode {
         drive = -gamepad1.left_stick_y;
         turn = gamepad1.right_stick_x;
 
-        if (gamepad1.dpad_left) {
-            strafePwr = 0.5;
-        }
-        else if (gamepad1.dpad_right) {
-            strafePwr = -0.5;
-        }
-        else {
-            strafePwr = 0;}
+        strafePwr = (gamepad1.dpad_left) ? (0.5) : (gamepad1.dpad_right) ? (-0.5) : (0);
 
-        hardware.strafe(strafePwr);
-        hardware.driveArcade(drive, turn);
+        m_strafe.strafe(strafePwr);
+        m_driveArcade.driveArcade(drive, turn);
 
         telemetry.addData("D-Pad left", gamepad1.dpad_left);
         telemetry.addData("D-Pad right", gamepad1.dpad_right);
