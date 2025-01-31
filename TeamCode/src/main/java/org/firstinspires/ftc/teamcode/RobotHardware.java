61e14cacf996a102d232d1fd7419fcdb26518ab2
Françoise Yen <francoiseyen@icloud.com>
2025-01-30 23:17:52 -0500

    TeleOp: all basic functions finished

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -3,15 +3,20 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class RobotHardware {
-    public static final double SERVO_CLOSED = 0.6, SERVO_OPEN = 0.7, MAX_PWR_DT = 1, MAX_PWR_LIFT = 1;
-    public static final int MAX_LIFT = 8400, MIN_LIFT = 0;
+    ElapsedTime timer = new ElapsedTime();
+    public static final double MAX_PWR_DT = 1, PWR_LIFT = 1, K_P = 0.05, K_I = 0.003, K_D = 0.003, PWR_CLAW = 0.3, SERVO_OPEN = 0.6, SERVO_CLOSED = 0.7;
+    public static final int MAX_LIFT = 8400, MIN_LIFT = 0, MAX_ARM = 420, MIN_ARM = 0, MAX_CLAW = 0, MIN_CLAW = -180;
+    public static double target, i, lastError;
     private final OpMode myOpMode;
     private DcMotorEx leftDrive = null;
     private DcMotorEx rightDrive = null;
     private DcMotorEx transverseDrive = null;
     private DcMotorEx liftMotor = null;
+    private DcMotorEx armMotor = null;
+    private DcMotorEx clawMotor = null;
     private Servo clawServo = null;
 
     public RobotHardware(OpMode opmode) {
@@ -19,6 +24,10 @@ public class RobotHardware {
     }
 
     public void init() {
+        target = 0;
+        i = 0;
+        lastError = 0;
+
         myOpMode.telemetry.addData(">", "Initializing");
         myOpMode.telemetry.update();
 
@@ -26,6 +35,8 @@ public class RobotHardware {
         rightDrive = myOpMode.hardwareMap.get(DcMotorEx.class, "right_drive");
         transverseDrive = myOpMode.hardwareMap.get(DcMotorEx.class, "transverse_drive");
         liftMotor = myOpMode.hardwareMap.get(DcMotorEx.class, "lift");
+        armMotor = myOpMode.hardwareMap.get(DcMotorEx.class, "arm");
+        clawMotor = myOpMode.hardwareMap.get(DcMotorEx.class, "claw_motor");
         clawServo = myOpMode.hardwareMap.get(Servo.class, "claw_servo");
 
         clawServo.setPosition(SERVO_CLOSED);
@@ -34,22 +45,30 @@ public class RobotHardware {
         rightDrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         transverseDrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         liftMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        clawMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
 
         leftDrive.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         rightDrive.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         transverseDrive.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         liftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        armMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        clawMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
 
         leftDrive.setDirection(DcMotorEx.Direction.FORWARD);
         rightDrive.setDirection(DcMotorEx.Direction.REVERSE);
         transverseDrive.setDirection(DcMotorEx.Direction.REVERSE);
         liftMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        armMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        clawMotor.setDirection(DcMotorEx.Direction.FORWARD);
         clawServo.setDirection(Servo.Direction.FORWARD);
 
         leftDrive.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         rightDrive.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         transverseDrive.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         liftMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        clawMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
 
         myOpMode.telemetry.addData(">", "Initialized");
         myOpMode.telemetry.update();
@@ -58,10 +77,6 @@ public class RobotHardware {
     public void setDrivePower(double left, double right) {
         leftDrive.setPower(left);
         rightDrive.setPower(right);
-    }
-
-    public void strafe(double pwr) {
-        transverseDrive.setPower(pwr);
     }
 
     public void driveArcade(double drive, double turn) {
@@ -77,41 +92,78 @@ public class RobotHardware {
         setDrivePower(leftPwr, rightPwr);
     }
 
-    public void moveClaw(boolean direction) {
-        if (direction) {
-            clawServo.setPosition(SERVO_CLOSED);
-        } else {
-            clawServo.setPosition(SERVO_OPEN);
-        }
+    public void strafe(double pwr) {
+        transverseDrive.setPower(pwr);
     }
 
-    public double moveLift(int state, boolean override) {
-        double pwr = 0;
-        /*switch (state) {
-            case 2:
-                if (getLift() < MAX_LIFT) {
-                    pwr = MAX_PWR_LIFT;
-                }
-            case 1:
-                if (getLift() > MIN_LIFT) {
-                    pwr = -MAX_PWR_LIFT;
-                }
-        }*/
-        if (state == 2 && (getLift() < MAX_LIFT || override)) {
-            pwr = MAX_PWR_LIFT;
-        }
-        if (state == 1 && (getLift() > MIN_LIFT || override)) {
-            pwr = -MAX_PWR_LIFT;
-        }
-        setLift(pwr);
-        return pwr;
-    }
-
-    public double getLift() {
+    public int getLift() {
         return liftMotor.getCurrentPosition();
     }
 
     public void setLift(double pwr) {
         liftMotor.setPower(pwr);
+    }
+
+    public void moveLift(int state, boolean override) {
+        double pwr = 0;
+
+        if (state == 2 && (getLift() < MAX_LIFT || override)) {
+            pwr = PWR_LIFT;
+        }
+        if (state == 1 && (getLift() > MIN_LIFT || override)) {
+            pwr = -PWR_LIFT;
+        }
+
+        setLift(pwr);
+    }
+
+    public int getArm() {
+        return armMotor.getCurrentPosition();
+    }
+
+    public void setArm(double pwr) {
+        armMotor.setPower(pwr);
+    }
+
+    public void moveArm(double rotation, boolean override) {
+        if (((rotation > 0 && getArm() < MAX_ARM) || (rotation < 0 && getArm() > MIN_ARM)) || override) {
+            target += rotation;
+        }
+        double error = target - getArm();
+        i += error * timer.seconds();
+
+        setArm((K_P * error) + (K_I * i) + (K_D * ((error - lastError) / timer.seconds())));
+
+        lastError = error;
+        timer.reset();
+    }
+
+    public int getClaw() {
+        return clawMotor.getCurrentPosition();
+    }
+
+    public void setClaw(double pwr) {
+        clawMotor.setPower(pwr);
+    }
+
+    public void rotateClaw(int state, boolean override) {
+        double pwr = 0;
+
+        if (state == 2 && (getClaw() < MAX_CLAW || override)) {
+            pwr = PWR_CLAW;
+        }
+        if (state == 1 && (getClaw() > MIN_CLAW || override)) {
+            pwr = -PWR_CLAW;
+        }
+
+        setClaw(pwr);
+    }
+
+    public void moveClaw(boolean state) {
+        if (state) {
+            clawServo.setPosition(SERVO_OPEN);
+        } else {
+            clawServo.setPosition(SERVO_CLOSED);
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 public class TeleOpMode extends OpMode {
     RobotHardware robot = new RobotHardware(this);
+    boolean clawState;
 
     @Override
     public void init() {
@@ -17,6 +18,7 @@ public class TeleOpMode extends OpMode {
         telemetry.addData(">", "OpMode active");
 
         boolean override = false;
+
         if (gamepad1.options) {
             override = true;
             telemetry.addData("!OVERRIDE", "ACTIVE!");
@@ -25,32 +27,34 @@ public class TeleOpMode extends OpMode {
         double drive = -gamepad1.left_stick_y;
         double turn = gamepad1.right_stick_x;
         double strafe = -gamepad1.left_stick_x;
-
-        if (gamepad1.left_bumper) {
-            robot.moveClaw(true);
-            telemetry.addData("Claw", "Open");
-        } else if (gamepad1.right_bumper) {
-            robot.moveClaw(false);
-            telemetry.addData("Claw", "Closed");
-        } else {
-            telemetry.addData("Claw", "Break");
-        }
+        double arm = gamepad1.left_trigger - gamepad1.right_trigger;
+        int liftState = 0, clawRState = 0;
 
         if (gamepad1.dpad_up && !gamepad1.dpad_down) {
-            telemetry.addData("Lift pwr", robot.moveLift(2, override));
+            liftState = 2;
         } else if (gamepad1.dpad_down && !gamepad1.dpad_up) {
-            telemetry.addData("Lift pwr", robot.moveLift(1, override));
-        } else {
-            telemetry.addData("Lift pwr", robot.moveLift(0, override));
+            liftState = 1;
         }
 
-        robot.strafe(strafe);
-        robot.driveArcade(drive, turn);
+        if (gamepad1.y && !gamepad1.a) {
+            clawRState = 2;
+        } else if (gamepad1.a && !gamepad1.y) {
+            clawRState = 1;
+        }
 
-        telemetry.addData("Drive", drive);
-        telemetry.addData("Turn", turn);
-        telemetry.addData("Strafe power", strafe);
-        telemetry.addData("Lift", robot.getLift());
+        if (gamepad1.left_bumper && !gamepad1.right_bumper) {
+            clawState = true;
+        } else if (gamepad1.right_bumper && !gamepad1.left_bumper) {
+            clawState = false;
+        }
+
+        robot.driveArcade(drive, turn);
+        robot.strafe(strafe);
+        robot.moveLift(liftState, override);
+        robot.moveArm(arm, override);
+        robot.rotateClaw(clawRState, override);
+        robot.moveClaw(clawState);
+
         telemetry.update();
     }
 }
