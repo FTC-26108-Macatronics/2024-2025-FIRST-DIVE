ad966da7fea6f8be871ef844fe06f256e2748b35
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2025-03-25 13:53:04 -0400

    added commands

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/teleop/arm/MoveArmDown.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.commands.teleop.arm;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.subsystems.ArmSubsystem;
+
+public class MoveArmDown extends CommandBase {
+
+    private boolean override;
+
+    private ArmSubsystem m_armSubsystem;
+
+    public MoveArmDown(ArmSubsystem m_armSubsystem) {
+        this.m_armSubsystem = m_armSubsystem;
+        addRequirements();
+
+    }
+
+    @Override
+    public void initialize() {
+        override = false;
+
+    }
+
+    @Override
+    public void execute() {
+
+        if (gamepad1.options) {
+            override = true;
+            telemetry.addData("!OVERRIDE", "ACTIVE!");
+        }
+
+        double arm = -gamepad1.left_trigger;
+        m_armSubsystem.moveArm(arm, override);
+
+    }
+    @Override
+    public void end(boolean interrupted) {
+
+        m_armSubsystem.moveArm(0, false);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+
+}
