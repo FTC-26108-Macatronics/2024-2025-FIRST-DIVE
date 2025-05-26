0be34be0e25ec76ca478f23d47ed33a7fa91a334
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2025-05-26 12:44:12 -0400

    added stff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionConstants.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class VisionConstants {
+    public static String CAMERA_NAME = "vision";
+
+}
